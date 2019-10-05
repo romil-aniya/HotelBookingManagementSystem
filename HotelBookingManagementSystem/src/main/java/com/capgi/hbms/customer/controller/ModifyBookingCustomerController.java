a232db8bb026853c0a12efa10ceea7a51fe8e3b7
work12 <work12@Romil>
2019-10-05 10:23:22 +0530

    All Classes Created

---
+++ HotelBookingManagementSystem/src/main/java/com/capgi/hbms/customer/controller/ModifyBookingCustomerController.java
@@ -0,0 +1,23 @@
+package com.capgi.hbms.customer.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ModifyBookingCustomerController
+ */
+public class ModifyBookingCustomerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ HotelBookingManagementSystem/src/main/java/com/capgi/hbms/customer/controller/RegisterCustomerController.java
@@ -0,0 +1,23 @@
+package com.capgi.hbms.customer.controller;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterCustomerController
+ */
+public class RegisterCustomerController extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
